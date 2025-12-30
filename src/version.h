6918169e05300642ed789af495c9de0a3a9803de
Aethersailor <22260104+Aethersailor@users.noreply.github.com>
2025-12-30 16:07:07 +0800

    chore: bump version to 1.0.6
    New features in this release:
    - Hardcoded default_external_config with multi-CDN fallback
    - Automatic fallback to default config if user config is empty
    - 4 CDN URLs for maximum availability (gcore/testingcf/cdn/raw.github)

--- src/version.h
+++ src/version.h
@@ -1,7 +1,7 @@
 #ifndef VERSION_H_INCLUDED
 #define VERSION_H_INCLUDED
 
-#define VERSION "1.0.5"
+#define VERSION "1.0.6"
 #define BUILD_ID "" // Will be replaced during Docker build
 
 #endif // VERSION_H_INCLUDED
